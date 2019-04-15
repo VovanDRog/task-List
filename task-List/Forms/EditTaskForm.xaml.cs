a62dc684b70e539edc97db1861800ca34b848499
VovanDRog <45601937+VovanDRog@users.noreply.github.com>
2019-04-15 09:34:01 +0300

    Feat: add connection  and send data to server

--- task-List/Forms/EditTaskForm.xaml.cs
+++ task-List/Forms/EditTaskForm.xaml.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace task_List.Forms
 {
-    /// <summary>
-    /// Логика взаимодействия для EditForm.xaml
-    /// </summary>
     public partial class EditForm : Window
     {
+        const int PORT = 1556;
+        const string ADDRESS = "127.0.0.1";
+        TcpClient client = null;
+        static NetworkStream stream = null;
+
+        bool isLoginÑorrect = false;
+        bool isPasswordÑorrect = false;
+
+        AuthForm authForm;
+
+
         public EditForm()
         {
             InitializeComponent();
@@ -80,6 +79,24 @@ namespace task_List.Forms
         private void Change_Click(object sender, RoutedEventArgs e)
         {
 
+            client = new TcpClient(ADDRESS, PORT);
+            stream = client.GetStream();
+
+            if (NewName.Text.Length < 4 || NewName.Text.Length > 20 && Task.Text.Length < 4 || Task.Text.Length > 20 && Description.Text != "")
+            {
+                try
+                {
+                    string[] data = new string[] { NewName.Text, Task.Text, Description.Text };
+                    inputFromAServer(5, data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Something is wrong. \n\n\n" + ex.ToString());
+                }
+            }
+
+
+
             if (NewName.Text.Length < 4 || NewName.Text.Length > 20)
             {
                 NewNameErrorLabel.Content = "Incorrect new name length";
@@ -87,7 +104,7 @@ namespace task_List.Forms
             }
             else
             {
-                NameErrorLabel.Visibility = Visibility.Hidden;
+                NewNameErrorLabel.Visibility = Visibility.Hidden;
             }
 
             if (Task.Text.Length < 4 || Task.Text.Length > 20)
@@ -118,7 +135,40 @@ namespace task_List.Forms
             {
                 NameErrorLabel.Visibility = Visibility.Hidden;
             }
-        }    
+        }
+
+
+
+        private bool inputFromAServer(int a, string[] data)
+        {
+            try
+            {
+                byte[] msg = new byte[1024];
+                BinaryWriter writer = new BinaryWriter(stream);
+                BinaryReader reader = new BinaryReader(stream);
+
+                writer.Write(a);
+                writer.Flush();
+
+                foreach (string value in data)
+                {
+                    writer.Write(value);
+                    writer.Flush();
+                }
+
+                client.Close();
+                writer.Close();
+                reader.Close();
+
+                //return ( Convert.ToBoolean( reader.ReadByte()));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh. We have a error \n\n" + ex);
+            }
+            return false;
+        }
+
 
     }
 }
